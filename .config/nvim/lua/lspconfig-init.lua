714e191025a8aa12f7cfcdeb521ffc5d70b90bcf
thal <thal@users.noreply.github.com>
2024-05-20 22:19:12 -0500

    nvim-lspconfig config for dts

--- .config/nvim/lua/lspconfig-init.lua
+++ .config/nvim/lua/lspconfig-init.lua
@@ -1,18 +1,26 @@
 -- LSP config. Adapted from github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
--- Default in nvim 0.10
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
-local lspconfig = require('lspconfig')
+local configs = require('lspconfig.configs')
+
+configs.dts = {
+    default_config = {
+        cmd = { 'python3', '/home/tom/code/dt-lsp/server.py'},
+        filetypes = {'dts'},
+        root_dir = function(buffer_dir)
+            return vim.fn.getcwd()
+        end
+    }
+}
+
+local nvim_lsp = require('lspconfig')
+
 local opts = { noremap=true, silent=true }
 
 local on_attach = function(_, bufnr)
     local opts = { buffer = bufnr }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    -- Default in nvim 0.10
-    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
@@ -32,12 +40,7 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
--- lspconfig.pyright.setup {
---     on_attach = on_attach,
---     capabilities = capabilities
--- }
-
-lspconfig.rust_analyzer.setup{
+nvim_lsp.rust_analyzer.setup{
   settings = {
     ['rust-analyzer'] = {
       diagnostics = {
@@ -49,9 +52,13 @@ lspconfig.rust_analyzer.setup{
   capabilities = capabilities
 }
 
-lspconfig.clangd.setup {
+nvim_lsp.clangd.setup {
     cmd = {"clangd", "--log=verbose"},
     on_attach = on_attach,
     capabilities = capabilities
 }
 
+nvim_lsp.dts.setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+}

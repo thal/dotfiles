95f2d8a2e52a73c91acf10c55070d3aedb3919fa
thal <thal@users.noreply.github.com>
2024-05-20 22:44:57 -0500

    fix next unused workspace script

--- .local/bin/nextunused.py
+++ .local/bin/nextunused.py
@@ -6,16 +6,20 @@ import sys
 def next():
     workspaces = i3.get_workspaces()
     next = 1
-    for ws in workspaces:
+    for ws in sorted(workspaces, key=lambda w: w.num):
         if next < ws.num:
             break
         else:
             next+=1
     return next
 
+i3 = Connection()
+
 if __name__ == "__main__":
-    i3 = Connection()
     if len(sys.argv) > 1 and sys.argv[1] == "move":
-        i3.command("move container to workspace number %d" % next() )
+        new = next()
+        i3.command("move container workspace number %d" % new )
+        # TODO: rename old workspace too!
+        i3.command("workspace number %d" % new )
     else:
         i3.command( "workspace number %d new" % next() )
